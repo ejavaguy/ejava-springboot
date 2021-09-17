584f7c4e31c1d121b744d8e2a9d72cfa71fed9d9
jim <ejavaguy@gmail.com>
2021-09-16 21:21:26 -0400

    added missing @Configuration annotation

--- app/app-config/hello-starter/src/main/java/info/ejava/examples/app/hello/HelloResourceAutoConfiguration.java
+++ app/app-config/hello-starter/src/main/java/info/ejava/examples/app/hello/HelloResourceAutoConfiguration.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+@Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(StdOutHello.class)
 @ConditionalOnResource(resources = "file:./hello.properties")
 @AutoConfigureBefore(HelloAutoConfiguration.class)
