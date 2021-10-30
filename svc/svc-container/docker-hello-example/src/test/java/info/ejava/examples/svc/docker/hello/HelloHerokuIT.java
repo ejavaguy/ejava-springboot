45e49e20d6d1e080eb2f40efa236e130cf3a4cb2
jim <ejavaguy@gmail.com>
2021-10-30 09:31:49 -0400

    added heroku tests

---
+++ svc/svc-container/docker-hello-example/src/test/java/info/ejava/examples/svc/docker/hello/HelloHerokuIT.java
@@ -0,0 +1,70 @@
+package info.ejava.examples.svc.docker.hello;
+
+import info.ejava.examples.common.web.ServerConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+import static org.assertj.core.api.BDDAssertions.*;
+
+@SpringBootTest(classes=ClientTestConfiguration.class,
+    webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles({"test","heroku"})
+@Slf4j
+public class HelloHerokuIT {
+    @Autowired
+    private RestTemplate anonymousUser;
+    @Autowired
+    private RestTemplate authnUser;
+    @Autowired
+    private String authnUsername;
+    @Autowired
+    private URI baseUrl;
+    private UriComponentsBuilder helloUrl;
+
+    @BeforeEach
+    void init() {
+        log.info("baseUrl={}", baseUrl);
+        helloUrl = UriComponentsBuilder.fromUri(baseUrl).path("api/hello")
+                .queryParam("name","{name}");
+    }
+
+    @Test
+    void can_contact_server() {
+        //given
+        String name="jim";
+        URI url = helloUrl.build(name);
+        RequestEntity<Void> request = RequestEntity.get(url).build();
+        //when
+        ResponseEntity<String> response = anonymousUser.exchange(request, String.class);
+        //then
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        then(response.getBody()).isEqualTo("hello, " + name);
+    }
+
+    @Test
+    void can_authenticate_with_server() {
+        //given
+        String name="jim";
+        URI url = helloUrl.build(name);
+        RequestEntity<Void> request = RequestEntity.get(url).build();
+        //when
+        ResponseEntity<String> response = authnUser.exchange(request, String.class);
+        //then
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        then(response.getBody()).isEqualTo("hello, " + name + " (from " + authnUsername + ")");
+    }
+}
---
+++ svc/svc-container/docker-hello-example/src/test/java/info/ejava/examples/svc/docker/hello/HelloLocalNTest.java
@@ -0,0 +1,68 @@
+package info.ejava.examples.svc.docker.hello;
+
+import info.ejava.examples.common.web.ServerConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(classes={DockerHelloExampleApp.class,
+        ClientTestConfiguration.class},
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Slf4j
+public class HelloLocalNTest {
+    @Autowired
+    private RestTemplate anonymousUser;
+    @Autowired
+    private RestTemplate authnUser;
+    @Autowired
+    private String authnUsername;
+    private UriComponentsBuilder helloUrl;
+
+    @BeforeEach
+    void init(@LocalServerPort int port) {
+        URI baseUrl = new ServerConfig().withPort(port).build().getBaseUrl();
+        log.info("baseUrl={}", baseUrl);
+        helloUrl = UriComponentsBuilder.fromUri(baseUrl).path("api/hello")
+                .queryParam("name","{name}");
+    }
+
+    @Test
+    void can_contact_server() {
+        //given
+        String name="jim";
+        URI url = helloUrl.build(name);
+        RequestEntity<Void> request = RequestEntity.get(url).build();
+        //when
+        ResponseEntity<String> response = anonymousUser.exchange(request, String.class);
+        //then
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        then(response.getBody()).isEqualTo("hello, " + name);
+    }
+
+    @Test
+    void can_authenticate_with_server() {
+        //given
+        String name="jim";
+        URI url = helloUrl.build(name);
+        RequestEntity<Void> request = RequestEntity.get(url).build();
+        //when
+        ResponseEntity<String> response = authnUser.exchange(request, String.class);
+        //then
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        then(response.getBody()).isEqualTo("hello, " + name + " (from " + authnUsername + ")");
+    }
+}
